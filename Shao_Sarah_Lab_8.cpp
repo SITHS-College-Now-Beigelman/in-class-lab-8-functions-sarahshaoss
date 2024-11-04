700301e71d07122754c22587ab63cd13d5ebc70b
Sarah <sarahs389@nycstudents.net>
2024-11-04 16:27:41 -0500

    y

--- Shao_Sarah_Lab_8.cpp
+++ Shao_Sarah_Lab_8.cpp
@@ -3,28 +3,13 @@
 //Lab 8
 
 #include <iostream>
+#include <cmath>
+#include <cctype>
+
 using namespace std;
 //shortcut
 
-double square(double x)
-// creates function to find the square of a number
-{
-    return x * x;
-    //times number by itself
-}
 
-double four (double x)
-//raises number to the fourth power
-{
-    return x* x* x *x;
-}
-
-double floor (double x)
-//floors a number
-{
-    return static_cast<int>(x);
-    //turns the double into and integer
-}
 
 int main()
 // main function to display the outputs
@@ -35,31 +20,27 @@ int main()
     cout << "Enter a floating point number:\n> ";
     cin >> x;
 
-    cout << "The square root of the number: " << square(x) << endl;
+    cout << "The square root of the number: " << sqrt(x) << endl;
     //uses the function square
-    cout << "The number raised to the 4th power: " << four(x) << endl;
+    cout << "The number raised to the 4th power: " << pow(x,4) << endl;
     //uses the function four
     cout << "The floor of the number: " << floor(x) << endl;
     //uses the function floor
-
-    return 0;
-    //ends program
-}
-
-int doubleMultiply(int a, int b)
-(
-    int product;
-    product - a * b * 2;
-    return product;
-)
-int main()
-(
+    
     int num1, num2;
     cout << "Enter two integers: ";
     cin >> num1 >> num2;
     cout << "Twice the product of the numbers is: "
     << doubleMultiply(num1, num2) << endl;
 
-    system("PAUSE");
     return 0;
-)
+    //ends program
+}
+
+int doubleMultiply (int a, int b)
+{
+    int product;
+    product = a * b * 2;
+    return product;
+}
+
